98478e29e5f7fc1d163a2ea70d091378e4f09bc3
Stuka <flow.990mz@gmail.com>
2015-10-17 22:13:13 +0900

    アイテムとぶつかったら、アイテムが消滅してデバッグログに「焼きそばGet！」なログが出る.

---
+++ Assets/Scripts/ItemScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter (Collider hit){
+		if (hit.CompareTag ("Player")) { 
+			if(this.tag=="ItemYakisoba"){
+				//YouGot1000Pt!
+				Debug.Log("Yakisoba Get!!!");
+			}
+			Destroy (gameObject); 
+		}
+	}
+}
