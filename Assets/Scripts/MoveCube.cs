2167694012eae44d438f1f7e7d9d66d9af985014
devloy <brn@microweb.jp>
2015-10-18 02:38:30 +0900

    マップを追加しました。

---
+++ Assets/Scripts/MoveCube.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCube : MonoBehaviour {
+	public float speed = 1.0f;
+
+	private Rigidbody rigidbody;
+	private Renderer renderer;
+	private bool IsWathced = false;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody = GetComponent<Rigidbody> () as Rigidbody;
+		rigidbody.velocity = new Vector3 (speed, 0, 0);
+		renderer = GetComponent<Renderer> () as Renderer;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.x > 4.0f) {
+			rigidbody.velocity = new Vector3 (-speed, 0, 0);
+		} else if (transform.position.x < -4.0f) {
+			rigidbody.velocity = new Vector3 (speed, 0, 0);
+		}
+		if (this.IsWathced) {
+			renderer.material.color = Color.blue;
+		} else {
+			renderer.material.color = Color.white;
+		}
+	}
+	public void Watched() {
+		Debug.Log ("Watched");
+		this.IsWathced = true;
+	}
+	public void NWatched() {
+		this.IsWathced = false;
+	}
+	/*
+	void OnTriggerEnter(Collider col) {
+		Debug.Log ("enter");
+		renderer.material.color = Color.blue;
+	}
+	void OnTriggerExit(Collider col) {
+		Debug.Log ("exit");
+		renderer.material.color = Color.white;
+	}*/
+}
