41bf7ecf2e25952f5cfc38cbe9c1315f54c76102
Stuka <flow.990mz@gmail.com>
2015-10-17 15:07:23 +0900

    [feature/hud_ingame] TimeとScoreの数値をスクリプトでいじれるようにした.

--- Assets/Scripts/HUD/HUDInGame.cs
+++ Assets/Scripts/HUD/HUDInGame.cs
@@ -1,11 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class HUDInGame : MonoBehaviour {
+	public Text scoreText;
+	public Text timeText;
 
 	// Use this for initialization
 	void Start () {
-	
+		scoreText.text = "Score: 0";
+		timeText.text = "Time: 0";
 	}
 	
 	// Update is called once per frame
